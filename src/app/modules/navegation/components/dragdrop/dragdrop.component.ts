27fda3e343a18f605923001e6fc2b19430f67072
diogoalmeidacunha <diogocunha588@gmail.com>
2019-10-25 15:26:57 +0100

    Request data

--- src/app/modules/navegation/components/dragdrop/dragdrop.component.ts
+++ src/app/modules/navegation/components/dragdrop/dragdrop.component.ts
@@ -44,6 +44,21 @@ export class DragdropComponent {
 
   handleClick(message){
     console.log(message);
+
+    const ELEMENT_DATA: PeriodicElement[] = [
+      {position: 1, name: 'Fundo Lorvao ', weight: 1.0079, symbol: 'H'},
+      {position: 2, name: 'Fundo Ovar Composto', weight: 4.0026, symbol: 'He'},
+      {position: 3, name: 'Fundo Aveiro', weight: 6.941, symbol: 'Li'},
+      {position: 4, name: 'Fundo Sintra Cidade', weight: 9.0122, symbol: 'Be'},
+      {position: 5, name: 'Fundo Alcatra Composto', weight: 10.811, symbol: 'B'},
+      {position: 6, name: 'Fundo Boavista', weight: 12.0107, symbol: 'C'},
+      {position: 7, name: 'Serie Sao Pedro ', weight: 14.0067, symbol: 'N'},
+      {position: 8, name: 'Unidade Composta Gondomar', weight: 15.9994, symbol: 'O'},
+      {position: 9, name: 'Fundo Gondomar', weight: 18.9984, symbol: 'F'},
+      {position: 10, name: 'Fundo Rio Tinto', weight: 20.1797, symbol: 'Ne'},
+    ];
+    this.dataSourceDiogo = ELEMENT_DATA;
+    
   }
 
 }
